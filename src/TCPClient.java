53781969ee84602d63a1ced6110a986cbdaf06e8
KhazarIbrahimov <khazaribrahimov@gmail.com>
2022-09-04 06:30:23 +0400

    second commit

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -2,25 +2,35 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class TCPClient {
-    Socket socket ;
-    static byte[] bytes;
 
+    static String link;
+    static String ip;
+    static int port;
 
-    public TCPClient() throws IOException {
-        while (true){
-            socket = new Socket("192.168.31.8",2222);
-            OutputStream outputStream = socket.getOutputStream();
-            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+    public static void main(String[] args) throws IOException {
+        System.out.println("please enter name: ");
+        User.name = new Scanner(System.in).nextLine();
+        System.out.println("please enter surname: ");
+        User.surname = new Scanner(System.in).nextLine();
 
-            byte[] bytes = Main.link.getBytes();
-            dataOutputStream.writeInt(bytes.length);
-            dataOutputStream.write(bytes);
-        }
+        System.out.println("Please enter the link of the document you want to send: ");
+        link = new Scanner(System.in).nextLine();
 
-//        dataOutputStream.write(bytes);
-//        dataOutputStream.close();
+        System.out.println("enter the port and ip address of the person you want to send the file to: ");
+        String serverInfo = new Scanner(System.in).nextLine();
+        String[] arr = serverInfo.split(":");
+        ip = arr[0];
+        port = Integer.parseInt(arr[1]);
+
+        Socket socket = new Socket(ip,port);
+        OutputStream outputStream = socket.getOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+        byte[] bytes = TCPClient.link.getBytes();
+        dataOutputStream.writeInt(bytes.length);
+        dataOutputStream.write(bytes);
     }
-
 }
